0cd5f377d5fa0f44815a43c418fafbcffb37058e
tanmay mathur <tanmay.mathur4@gmail.com>
2018-06-01 02:01:32 -0700

    update

---
+++ array.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>h
+#include<conio.h>
+int main()
+{
+	int a[3][3],b[3][3],c[3][3],i,j,k;
+	printf("Enter the elements of first matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter the elements of second matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			 c[i][j]=0;
+			for(k=0;k<3;k++)
+			{
+				c[i][j]=c[i][j]+a[i][k]*b[k][j];
+			}
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\n",c[i][j]);
+		}
+	}
+	return 0;
+}
